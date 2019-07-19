b57ec25e43073962bc446960f62545a8930d976c
npalexey <schierke.schierke@gmail.com>
2019-07-19 15:42:26 +0300

    Service Starter v2

--- src/main/java/com/nikitiuk/booleanparsercontraption/service/ServiceStarter.java
+++ src/main/java/com/nikitiuk/booleanparsercontraption/service/ServiceStarter.java
@@ -1,19 +1,27 @@
 package com.nikitiuk.booleanparsercontraption.service;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ServiceStarter {
+    /*public static void main(String[] args) {
+        Map<String, Object> variablesMap = new HashMap<>();
+        variablesMap.put("a", true);
+        parseBoolExpr("a", variablesMap);
+    }*/
     public static Boolean parseBoolExpr(String expr){
-        if(Contraption.run(expr) != null){
-            return Contraption.run(expr);
+        Boolean result = Contraption.run(expr);
+        if(result != null){
+            return result;
         } else {
             System.out.println("Returned Null");
             return false;
         }
     }
-    public static Boolean parseBoolExpr(String expr, Map<String, Double> valuesMap){
-        if(Contraption.run(expr,valuesMap) != null){
-            return Contraption.run(expr,valuesMap);
+    public static Boolean parseBoolExpr(String expr, Map valuesMap){
+        Boolean result = Contraption.run(expr,valuesMap);
+        if(result != null){
+            return result;
         } else {
             System.out.println("Returned Null");
             return false;
