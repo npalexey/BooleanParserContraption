e374c010ef6c4b338db94b52a08dade7fbe0f438
npalexey <schierke.schierke@gmail.com>
2019-07-19 11:13:51 +0300

    Service Starter for lib

--- src/main/java/com/nikitiuk/booleanparsercontraption/service/ServiceStarter.java
+++ src/main/java/com/nikitiuk/booleanparsercontraption/service/ServiceStarter.java
@@ -3,7 +3,7 @@ package com.nikitiuk.booleanparsercontraption.service;
 import java.util.Map;
 
 public class ServiceStarter {
-    public static boolean parseBoolExpr(String expr){
+    public static Boolean parseBoolExpr(String expr){
         if(Contraption.run(expr) != null){
             return Contraption.run(expr);
         } else {
@@ -11,7 +11,7 @@ public class ServiceStarter {
             return false;
         }
     }
-    public static boolean parseBoolExpr(String expr, Map<String, Double> valuesMap){
+    public static Boolean parseBoolExpr(String expr, Map<String, Double> valuesMap){
         if(Contraption.run(expr,valuesMap) != null){
             return Contraption.run(expr,valuesMap);
         } else {
